812fa4e29bbebdb2b1318d6dab4cd36c2a12d31b
Vajinepalli Bhargav <vajinepallibhargav2004@gmail.com>
2026-02-12 15:04:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4207740462-dfb73",
+  "appId": "1:923405798446:web:83c10300376f15c44763bf",
+  "apiKey": "AIzaSyAoGmmsVwSsrW4JvTkPThPrAUEnlM87NBE",
+  "authDomain": "studio-4207740462-dfb73.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "923405798446"
+};
